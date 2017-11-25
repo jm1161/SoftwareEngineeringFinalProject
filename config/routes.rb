64ffc2866826c475c02271171e4aeb2f1211832c
jm1161 <32186001+jm1161@users.noreply.github.com>
2017-11-25 01:22:28 -0600

    added new controllers for buying/selling routes
    added their respective routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :views
-  get '/', to: "home#index"
-  get 'home/index'
 
-  devise_for :users
-  get "/admin/index", to: "admin#index"
+  	devise_for :views
+  	get '/', to: "home#index"
+  	get '/buy_books/index', to: "buy_books#index"
+  	get '/sell_books/index', to: "sell_books#index"
+
+  	devise_for :users
+  	get "/admin/index", to: "admin#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
